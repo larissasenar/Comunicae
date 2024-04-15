d1172a7a8dfb96ebfd10274aae5cdfdde7bbf4ff
Larissa Sena <larissasenar@gmail.com>
2024-04-15 17:02:19 -0300

    Update index.htm e style.css
    Alteração no conteúdo do site

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*menu hamburguer*/
+/*menu hamburguer
 
 const menuHamburguer = document.querySelector('.menu-hamburguer');
 menuHamburguer.addEventListener('click', () => {
@@ -15,3 +15,21 @@ function toggleMenu() {
         nav.style.display = 'none';
     }
 }
+*/
+
+/*menu-mobile teste*/
+
+const menuMobile = document.querySelector('.menu-mobile');
+const menuOverlay = document.querySelector('.menu-overlay');
+const sidebar = document.querySelector('.sidebar');
+const body = document.querySelector('body');
+
+menuMobile.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar-active');
+    body.classList.toggle('sidebar-active');
+});
+
+menuOverlay.addEventListener('click', () => {
+    sidebar.classList.remove('sidebar-active');
+    body.classList.remove('sidebar-active');
+});
